feddbdebcc049dc602da909b0d417b365812b928
이상은 <118444536+happyeun@users.noreply.github.com>
2023-05-30 21:07:26 +0900

    Update deploy2.sh 오타수정2

--- ubuntu/deploy2.sh
+++ ubuntu/deploy2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# [1] env variable 
+# [1] env variable
 # (환경변수 설정하는 스크립트 var.sh 실행)
 source ./var.sh
 echo $GITHUB_ID
@@ -14,24 +14,24 @@ rm crontab_delete
 echo "2. cron delete complete"
 
 # [3] server checking
-# (PROJECT_PID가 있으면 => 실행되고 있으면, true)
+# (PROJECT_가 있으면 => 실행됙 있으면, true)
 if [ -n "${PROJECT_PID}" ]; then
-  # re deploy
-  kill -9 $PROJECT_PID
-  echo "3. project kill complete"
+        # re deploy
+        kill -9 $PROJECT_PID
+        echo "3. project kill complete"
 else
-  # first deploy
-  # 3-1 apt update
-  sudo apt-get -y update 1>/dev/null
-  echo "3-1. apt update complete"
-  
-  # 3-2 jdk install
-  sudo apt-get -y install openjdk-11-jdk 1>/dev/null
-  echo "3-2. jdk install complete"
-  
-  # 3-3 timezone
-  sudo timedatectl set-timezone Asia/Seoul
-  echo "3-3. timezone setting complete"
+        # first deploy
+        # 3-1. apt update
+        sudo apt-get -y update 1>/dev/null
+        echo "3-1. apt update complete"
+
+        # 3-2 jdk install
+        sudo apt-get -y install openjdk-11-jdk 1>/dev/null
+        echo "3-2. jdk install complte"
+
+        # 3-3 timezone
+        sudo timedatectl set-timezone Asia/Seoul
+        echo "3-3. timezone setting complete"
 fi
 
 # [4] project folder delete
@@ -49,17 +49,18 @@ echo "6. gradlew u+x complete"
 
 # [7] build
 cd ${HOME}/${PROJECT_NAME}
-./gradlew clean build
+./gradlew build
 echo "7. gradlew build complete"
 
-# [8] start jar
-nohup java -jar -Dspring.profiles.active=prod ${JAR_PATH} 1>${HOME}/log.out 2>${HOME}/err.out &
-echo "8. start server complete"
+# [8] start  jar
+        # 개발 코드와   운영 코드의  포트번호를 나누어 놓았다.
+nohup java -jar -Dspring.profiles.active=prod ${JAR_PATH} 1>>${HOME}/log.out 2>>${HOME}/err.out &
+echo "8. star server complete"
 
 # [9] cron registration
 touch crontab_new
 echo "* * * * * ${HOME}/check-and-restart.sh" 1>>crontab_new
-# register the others... you use >> (append)
+# register the other... you use >> (append)
 crontab crontab_new
 rm crontab_new
-echo "9. cron registration complete"
+echo "9. cron registration complete "
